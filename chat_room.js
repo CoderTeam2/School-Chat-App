8a610cd64d236728184f9b35038b2d3183838f87
CoderTeam2 <72189661+CoderTeam2@users.noreply.github.com>
2021-08-29 12:32:06 +0530

    Delete chat_room.js

--- chat_room.js
+++
@@ -1,6 +0,0 @@
-var name = localStorage.getItem("User");
-document.getElementById("greeting").innerHTML = "Welcome "+name+"!"
-function logout(){
-    localStorage.removeItem("User");
-    window.location = "index.html";
-}
